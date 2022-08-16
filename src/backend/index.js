b2e5e238c6975be77f6824f95868e2a92127c5d8
Norberto Rodriguez <norberto1976@hotmail.com>
2022-08-16 14:08:04 -0300

    Se agrega el delete y el insert  En la rama practico  Tu rama está actualizada con 'origin/practico'.
     Cambios a ser confirmados:
    	modificados:     src/backend/index.js

--- src/backend/index.js
+++ src/backend/index.js
@@ -41,28 +41,49 @@ app.post("/actualizar",function(req,res){
    
 });
 
-//post para borrar un dispositivo
+//delete para borrar un dispositivo
 app.delete("/eliminar",function(req,res){
     console.log("Llegue al servidor para borrar")
     console.log(Object.keys(req.body).length)
-    if(req.body.id!=undefined){
+    
+    /*if(req.body.id!=undefined){
         console.log(req.body);
         res.send("eliminado");
     }else{
         res.send("ERROR");
-    }
+    }*/
+
+    //elimina el dispositivo de la base de datos
+    let sql= "delete from Devices where id=?";
+    utils.query(sql,[req.body.id],function(err,respuesta){
+        if(err){
+            res.send(err).status(400);
+            return;
+        }
+        res.send(respuesta)
+    });
 });
 
 //post para agregar un dispositivo
 app.post("/agregar",function(req,res){
     console.log("Llegue al servidor para agregar")
     console.log(Object.keys(req.body).length)
-    if(req.body.id!=undefined){
+    /*if(req.body.id!=undefined){
         console.log(req.body);
         res.send("agregado");
     }else{
         res.send("ERROR");
-    }
+    }*/
+
+    //se agrega un dispositivo a la base de datos
+    let sql= "INSERT INTO Devices (id, name, description, state, type) VALUES (?, ?, ?, ?, ?)";
+    utils.query(sql,[req.body.id,req.body.name,req.body.description,req.body.state,req.body.type],function(err,respuesta){
+        if(err){
+            res.send(err).status(400);
+            return;
+        }
+        res.send(respuesta)
+    });
 });
 
 app.get('/devicesDB/', function(req, res) {
